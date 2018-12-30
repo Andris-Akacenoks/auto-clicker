ff830f5826e216260e03e5d6ac20bc13177c3f95
andris.akacenoks <andris.akacenoks@testdevlab.com>
2018-12-31 00:01:34 +0200

    Minor improvements

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,13 @@
 package sample;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
 
-public class Controller{
+public class Controller {
     @FXML public Button startClicking ;
     @FXML public Button stopClicking ;
     @FXML public TextField frequencyInMilliseconds;
@@ -20,15 +17,24 @@ public class Controller{
         activateClicking();
         startClicking.setDisable(true);
         stopClicking.setDisable(false);
+        performClicking();
+    }
+
+    public void handleStopClicking(){
+        disableClicking();
+        startClicking.setDisable(false);
+        stopClicking.setDisable(true);
+    }
+
+    private void performClicking(){
         new Thread(new Runnable() {
             public void run() {
                 Robot robot = setRobot();
                 int frequency = getFrequency();
-                do{
+                do {
                     if (isClickingActive){
                         robot.mousePress(InputEvent.BUTTON1_MASK);
                         robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                        System.out.println("Click performed");
                         try {
                             Thread.sleep(frequency);
                         } catch (InterruptedException e) {
@@ -36,21 +42,11 @@ public class Controller{
                         }
                         PointerInfo a = MouseInfo.getPointerInfo();
                         Point b = a.getLocation();
-                        int xOrig = (int) b.getX();
-                        int yOrig = (int) b.getY();
-                        robot.mouseMove(xOrig, yOrig);
+                        robot.mouseMove((int) b.getX(), (int) b.getY());
                     }
-                }
-                while(isClickingActive);
+                } while(isClickingActive);
             }
         }).start();
-    }
-
-    public void handleStopClicking(){
-        System.out.println("Clicking stopped");
-        disableClicking();
-        startClicking.setDisable(false);
-        stopClicking.setDisable(true);
     }
 
     private void activateClicking(){
@@ -64,9 +60,10 @@ public class Controller{
     private int getFrequency(){
         try {
             int frequency = Integer.valueOf(frequencyInMilliseconds.getText());
-            if (frequency > 50 && frequency <= 60 * 1000) {
+            if (frequency >= 50 && frequency <= 60 * 1000) {
                 return frequency;
             } else {
+                frequencyInMilliseconds.setText(String.valueOf(1000));
                 return 1000;
             }
         } catch (IllegalArgumentException e) {
